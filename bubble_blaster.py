98a19d2d8330dc0c5ea5a23c89b2ff533f23a543
Noeliala14 <noelialgomez@hotmail.com>
2026-02-01 19:11:29 +0100

    Draw a triangle for the submarine body

--- bubble_blaster.py
+++ bubble_blaster.py
@@ -19,3 +19,21 @@ window.title("Bubble Blaster - Python Learning Project")
 c = tk.Canvas(window, width=WIDTH, height=HEIGHT, bg="darkblue")
 c.pack()
 
+# Draw a triangle for the submarine body
+# ID for tracking the polygon shape
+sub_id = c.create_polygon(5, 5, 5, 25, 30, 15, fill="red")
+
+# Draw the outline of a circle (the submarine's hull)
+sub_id2 = c.create_oval(0, 0, 30, 30, outline="white")
+
+# Submarine radius for collision detection logic
+SUB_R = 15
+
+# Calculate the center coordinates of the sreen
+MID_X = WIDTH / 2
+MID_Y = HEIGHT / 2
+
+# Move both parts of the submarine to the center
+c.move(sub_id, MID_X, MID_Y)
+c.move(sub_id2, MID_X, MID_Y)
+
